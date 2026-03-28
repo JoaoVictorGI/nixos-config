cdf40d5b389a72fdff74de4a219308a6174fdf09
Joao <joao@example.com>
2026-03-28 22:55:13 +0000

    refactor: remove treefmt.nix and add devShell with nixfmt-tree

--- modules/flake-parts.nix
+++ modules/flake-parts.nix
@@ -1,4 +1,13 @@
-{ inputs, ... }: {
+{ inputs, ... }:
+{
   imports = [ inputs.flake-parts.flakeModules.modules ];
   systems = [ "x86_64-linux" ];
+
+  perSystem =
+    { pkgs, ... }:
+    {
+      devShells.default = pkgs.mkShell {
+        packages = [ pkgs.nixfmt-tree ];
+      };
+    };
 }
--- treefmt.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-
-{
-  projectRootFile = "flake.nix";
-  programs = {
-    nixfmt.enable = true;
-  };
-}
