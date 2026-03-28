2edd7465372704e5a0ca8d2d0f55692daef6f2a4
João Victor <joaovictor.gizidoro@gmail.com>
2026-03-28 03:06:00 +0000

    feat: add agenix for secrets management

--- flake.nix
+++ flake.nix
@@ -1,14 +1,51 @@
 {
-  input = {
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
-    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    emacs = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    agenix.url = "github:ryantm/agenix";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake
-    { inherit inputs; }
-    (inputs.import-tree ./modules);
+  outputs =
+    inputs:
+    let
+      tree = inputs.import-tree ./modules;
+      baseModule = {
+        systems = [ "x86_64-linux" ];
+        imports = [
+          inputs.flake-parts.flakeModules.modules
+          inputs.home-manager.flakeModules.home-manager
+        ];
+      };
+    in
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      baseModule
+      // tree
+      // {
+        perSystem =
+          { config, pkgs, ... }:
+          {
+            formatter = (inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build.wrapper;
+          };
+      }
+    );
 }
