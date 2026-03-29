d25cc086fe0d8aa995f02e0a5113ab00ec1d5808
João Victor <joaovictor.gizidoro@gmail.com>
2026-03-29 02:14:43 +0000

    fix: confire gpg-agent correctly

--- modules/programs/gpg.nix
+++ modules/programs/gpg.nix
@@ -14,6 +14,7 @@
             maxCacheTtl = 34560000;
             maxCacheTtlSsh = 34560000;
             pinentry.package = pkgs.pinentry-gtk2;
+            sshKeys = [ "C7E24DA262329549A1EBC7CC59B18690BE37E1CB" ];
           };
         }
       ];
